b137b7febceba19533bb0495688ec3f0154abdc5
AdrijaDastidar <adrijadastidar@gmail.com>
2024-06-28 21:33:27 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
+import News from "./components/News";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <>
         <Navbar title="Readico" />
-      </div>
+        <div className="container">
+          <News />
+        </div>
+      </>
     );
   }
 }
---
+++ src/components/NewsItems.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class NewsItems extends Component {
+  render() {
+    return (
+      <div className="container">
+        <p>This is a news</p>
+      </div>
+    );
+  }
+}
+
+export default NewsItems;
