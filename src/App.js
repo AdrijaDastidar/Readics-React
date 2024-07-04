9c7908f3337f9b59b5b33faeaba2a2a4c93f15ec
AdrijaDastidar <adrijadastidar@gmail.com>
2024-07-04 20:50:29 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default class App extends Component {
         <div className="container">
           <Routes>
             <Route
-              path="/"
+              path="/Readics-React"
               element={
                 <News
                   heading="Top Headlines"
